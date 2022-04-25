44a7417c3a3badbeed4f26f7facf1067e415e047
Alejandro Muñoz Letayf <alexletayf@gmail.com>
2022-04-25 15:59:51 -0500

    Solved JS101 Easy 1 exercise 4 Further Exploration

--- exercises/JS101_Easy_1_4.js
+++ exercises/JS101_Easy_1_4.js
@@ -1,21 +1,51 @@
 const rlSync = require('readline-sync');
 const SQMETERS_TO_SQFEET = 10.7639;
 
+//Asking for measurement type
+console.log('Write meters or feet to choose your measurement type.')
+let measureType = '';
+
+do {
+	measureType = rlSync.prompt();
+}
+while (!(measureType == 'meters' || measureType == 'feet'))
+
+
 //Asking for the length
-console.log('Enter the length of the room in meters:');
+console.log(`Enter the length of the room in ${measureType}:`);
 let roomLength = rlSync.prompt();
+roomLength = parseInt(roomLength);
+
 
 //Asking for the width
-console.log('Enter the width of the room in meters:');
+console.log(`Enter the width of the room in ${measureType}:`);
 let roomWidth = rlSync.prompt();
+roomWidth = parseInt(roomWidth);
 
+//Conversion functions
 function convertMetersToFeet(meters) {
 	return meters * SQMETERS_TO_SQFEET;
 }
 
-let areaMeters = roomLength * roomWidth;
+function convertFeetToMeters(feet) {
+	return feet / SQMETERS_TO_SQFEET;
+}
 
-let areaFeet = convertMetersToFeet(areaMeters);
+//Calculate area
+let area = roomWidth * roomLength;
 
-//Logging to the console the answer
-console.log(`The area of the room is ${areaMeters} square meters (${areaFeet} square feet).`);
+//Calculate alternative area
+let areaConversion;
+
+if (measureType == 'meters') { 
+	areaConversion = area * SQMETERS_TO_SQFEET;
+} else {
+	areaConversion = area / SQMETERS_TO_SQFEET;
+}
+
+//Result
+if (measureType == 'meters') {
+	console.log(`The area of the room is ${area} square meters (${areaConversion} square feet).`);
+} else {
+	console.log(`The area of the room is ${area} square feet (${areaConversion} square meters).`);
+}
