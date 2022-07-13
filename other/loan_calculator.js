aec5fb2c25b717a9b71141e4d5a09254bad99d5e
Alejandro Muñoz Letayf <alexletayf@gmail.com>
2022-07-12 20:24:57 -0500

    Improved Loan Calcultaror from Lesson 2 Assignment 19

--- other/loan_calculator.js
+++ other/loan_calculator.js
@@ -1,12 +1,45 @@
-const read = require('readline-sync');
+//External modules
+let read = require(`readline-sync`);
 
-let loanAmount = read.question("What's your loan amount?\n");
-let annualPorcentageRate = read.question("What's your APR?\n");
-let months = read.question("Desired # of months?\n");
-let monthlyRate = annualPorcentageRate / 12;
+console.clear();
 
-let monthlyPayment = loanAmount * (monthlyRate / /*...*/
-  (1 - Math.pow((1 + monthlyRate), (-months))));
+let welcomeMessage = `Welcome to Alex's Loan Calculator.\n\
+Follow the instructions to calculate your monthly payment.\n`;
 
-console.log(monthlyPayment);
+function isInvalidNumber(number) {
+  return number.trim() === '' ||
+         Number(number) < 0   ||
+         Number.isNaN(Number(number));
+}
 
+function ask(questionText) {
+  let response = ``;
+
+  do {
+    console.clear();
+    console.log(welcomeMessage);
+    response = read.question(`${questionText}\n>> `);
+  } while (isInvalidNumber(response));
+
+  return Number(response);
+}
+
+let loanAmount = ask(`What is your loan amount?`);
+
+let monthlyInterestRate = ask(`What is your yearly interest rate?\n\n\
+Write the rate as a decimal.`) / 12;
+
+let durationMonths = ask(`What is the duration of your loan in months?`);
+
+let monthlyPayment = loanAmount * (monthlyInterestRate /
+  (1 - Math.pow((1 + monthlyInterestRate), (0 - durationMonths))));
+
+console.clear();
+
+console.log(`Thank you for using Alex's Loan Calculator.\n\n\
+Considering:\n\
+1 A loan amount of ${loanAmount.toFixed(2)}\n\
+2 A yearly interest rate of ${(monthlyInterestRate * 12).toFixed(2)}\n\
+3 A loan duration of ${durationMonths} months \n`);
+
+console.log(`Your monthly payment should be $${monthlyPayment.toFixed(2)}\n`);
