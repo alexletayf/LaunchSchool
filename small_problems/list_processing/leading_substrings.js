cc570143120c051f3a6982c026e105bfa328c9c1
Alejandro Muñoz Letayf <alexletayf@gmail.com>
2024-04-03 00:15:50 -0600

    Solved Leading substrings

---
+++ small_problems/list_processing/leading_substrings.js
@@ -0,0 +1,12 @@
+function leadingSubstrings(text) {
+  let rtnArr = [];
+  let textArr = text.split('');
+  for (let i = 1; i < text.length + 1; i++) {
+      rtnArr.push(text.slice(0,i));
+  }
+  return rtnArr;
+}
+
+const sample = 'ABC';
+
+console.log(leadingSubstrings(sample));
